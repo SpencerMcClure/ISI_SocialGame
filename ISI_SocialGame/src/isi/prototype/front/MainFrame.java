77a11e97b78d02667d4baac9aff966bf39576fc6
Spencer McClure <spencer.mcclure@comcast.net>
2012-04-01 16:51:24 -0700

    Added some details

--- ISI_SocialGame/src/isi/prototype/front/MainFrame.java
+++ ISI_SocialGame/src/isi/prototype/front/MainFrame.java
@@ -1,5 +1,23 @@
 package isi.prototype.front;
 
-public class MainFrame {
+import java.util.List;
+
+import isi.prototype.back.Connection;
+import isi.prototype.back.Jammer;
+import isi.prototype.back.Node;
+import isi.prototype.shared.FrontEnd;
+
+public class MainFrame implements FrontEnd{
+
+	public void msgGameOver() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void msgHereIsLayout(List<Node> nodes, List<Jammer> jammers,
+			List<Connection> connections) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- ISI_SocialGame/src/isi/prototype/shared/BackEnd.java
+++ ISI_SocialGame/src/isi/prototype/shared/BackEnd.java
@@ -5,9 +5,12 @@ import isi.prototype.back.Jammer;
 
 public interface BackEnd {
 	
+	//Will be told by FrontEnd when values change
+	//This is really only of use to the backend for maintaining up-to-date states
+	//And for collecting data with regards to how the game is played by users
 	public void msgJammerValueChange(Jammer j, int i);
 	public void msgConnectionValueChanged(Connection c, int i);
-	public void msgConnectionBlocked(Connection c);
+	
 	public void msgHereIsFrontEnd(FrontEnd f);
 	
 	public void gameBeginning();
