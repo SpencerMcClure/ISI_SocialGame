0143a98461b1cff3e3c47a1bdcb34b9431ab45c1
Spencer McClure <spencer.mcclure@comcast.net>
2012-04-26 14:25:39 -0700

    upated image

--- ISI_Social_Game/src/com/front/client/FrontJammer.java
+++ ISI_Social_Game/src/com/front/client/FrontJammer.java
@@ -13,7 +13,8 @@ public class FrontJammer extends GameObject{
 	static String src = "images/jammer.png";
 	static ImageElement img;
 	static int width, height;
-	
+	private int radius;
+	final double PI = 3.14159265;
 	
 	
 	//FrontJammer.JammerImage img = new JammerImage();
@@ -25,7 +26,18 @@ public class FrontJammer extends GameObject{
 		width = size;
 		height = size;
 		type = Class.JAMMER;
-		
+		radius=90;
+		this.z = 85;
+		//img.setSrc(src);
+	}
+	
+	public FrontJammer(Context2d c, int x, int y,int radius, EventLog l)
+	{
+		super(c,x,y,l);
+		width = size;
+		height = size;
+		type = Class.JAMMER;
+		this.radius = radius;
 		this.z = 85;
 		//img.setSrc(src);
 	}
@@ -38,7 +50,15 @@ public class FrontJammer extends GameObject{
 
 	@Override
 	public void draw() {
+		c.setStrokeStyle("#A7A6A6");
+		c.setFillStyle("#FEF3F3");
+		c.setLineWidth(4);
 		c.translate(loc.x, loc.y);
+		c.beginPath();
+		c.arc(0, 0, radius, 2*PI, 0);
+		c.closePath();
+		c.stroke();
+		c.fill();
 		c.drawImage(img, -FrontJammer.width/2, -FrontJammer.height/2);
 		c.translate(-loc.x, -loc.y);
 		
